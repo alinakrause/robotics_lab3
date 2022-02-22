c5e2ac15715278cf68d6cfb05df33937beacac30
alinakrause <alikrause@uni-osnabrueck.de>
2022-02-22 02:07:18 +0000

    test cases

--- robotics_lab3/robot_config.py
+++ robotics_lab3/robot_config.py
@@ -4,4 +4,47 @@ import numpy as np
 
 
 if __name__ == '__main__':
-	robot_model.kinematic_chain([[np.pi/2,1.0,0,0],[np.pi/2,1.0,0,0]])
+	#calculate total homogenous transformation
+	a = robot_model.kinematic_chain([[np.pi/2,1.0,0.0,0.0],[np.pi/2,1.0,0.0,0.0]])
+	#calculate x,y,z components of position
+	pos = robot_model.get_pos(a)
+	#calculate roll-pitch-yaw angles
+	rot = robot_model.get_rot(a)
+	print('two-link planar')
+	print('x: %.2f' %pos[0])
+	print('y: %.2f' %pos[1])
+	print('z: %.2f' %pos[2])
+	print('pitch: %.2f' %rot[1])
+	print('roll: %.2f' %rot[2])
+	print('yaw: %.2f' %rot[0])
+	
+	#calculate total homogenous transformation
+	b = robot_model.kinematic_chain([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+	#calculate x,y,z components of position
+	pos = robot_model.get_pos(b)
+	#calculate roll-pitch-yaw angles
+	rot = robot_model.get_rot(b)
+	print('6 DoF UR5e case 1')
+	print('x: %.2f' %pos[0])
+	print('y: %.2f' %pos[1])
+	print('z: %.2f' %pos[2])
+	print('pitch: %.2f' %rot[1])
+	print('roll: %.2f' %rot[2])
+	print('yaw: %.2f' %rot[0])
+	
+	#calculate total homogenous transformation
+	c = robot_model.kinematic_chain([[0,0,0,0],[-np.pi/2,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+	#calculate x,y,z components of position
+	pos = robot_model.get_pos(c)
+	#calculate roll-pitch-yaw angles
+	rot = robot_model.get_rot(c)
+	print('6 Dof UR5e case 2')
+	print('x: %.2f' %pos[0])
+	print('y: %.2f' %pos[1])
+	print('z: %.2f' %pos[2])
+	print('pitch: %.2f' %rot[1])
+	print('roll: %.2f' %rot[2])
+	print('yaw: %.2f' %rot[0])
+	
+	
+
--- robotics_lab3/robot_model.py
+++ robotics_lab3/robot_model.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 
 def dh_transformation(theta, a, d, alpha):
+	#receives DH parameters and returns combined homogenous transformation
 	dh = [[np.cos(theta),-np.sin(theta)*np.cos(alpha),np.sin(theta)*np.sin(alpha), a*np.cos(theta)],
 	[np.sin(theta),np.cos(theta)*np.cos(alpha),-np.cos(theta)*np.sin(alpha), a*np.sin(theta)],
 	[0.0, np.sin(alpha), np.cos(alpha), d],
@@ -9,13 +10,16 @@ def dh_transformation(theta, a, d, alpha):
 	return dh
 
 def kinematic_chain(dh_param):
+	#receives DH parameters and returns total homogenous transformation for the kinematic chain
 	trans = np.identity(4)
 	for x in dh_param:
 		dh = dh_transformation(x[0],x[1],x[2],x[3])
 		trans = np.matmul(trans,dh)
 	return trans
 
+
 def get_pos(trans):
+	#returns x,y,z coordinates
 	x = trans[0][3]
 	y = trans[1][3]
 	z = trans[2][3]
@@ -23,6 +27,7 @@ def get_pos(trans):
 	return pos
 
 def get_rot(trans):
+	#returns pitch roll yaw values 
 	phi = np.arctan(trans[1][0]/trans[0][0])
 	theta = np.arctan((-1*trans[2][0])/(np.sqrt((trans[2][1]*trans[2][1])+(trans[2][2]*trans[2][2]))))
 	psi = np.arctan(trans[2][1]/trans[2][2])
